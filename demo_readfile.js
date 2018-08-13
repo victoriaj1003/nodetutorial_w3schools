70bbc110b3115a7652f0c2db0320e3e8f325a8ec
Victoria J Boggs <victoriaboggs@Victorias-MacBook.local>
2018-08-12 21:01:46 -0400

    added new files for fileserve and url

--- demo_readfile.js
+++ demo_readfile.js
@@ -6,4 +6,40 @@ http.createServer(function (req, res) {
     res.write(data);
     res.end();
   });
+
+  fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+  fs.open('mynewfile2.txt', 'w', function (err) {
+    if (err) throw err;
+    console.log('Saved2!');
+  });
+
+  fs.writeFile('mynewfile3.txt', 'Hello content 3!', function (err) {
+    if (err) throw err;
+    console.log('Saved3!');
+  });
+
+  fs.appendFile('mynewfile1.txt', 'This is my text', function (err) {
+    if (err) throw err;
+    console.log('Updated!');
+  });
+
+  fs.writeFile('mynewfile3.txt', 'This is new content', function (err) {
+    if (err) throw err;
+    console.log('Replaced!');
+  });
+
+  fs.unlink('mynewfile2.txt', function (err) {
+    if (err) throw err;
+    console.log('File deleted!');
+  });
+
+  fs.rename('mynewfile1.txt', 'myrenamedfile1.txt', function (err) {
+    if (err) throw err;
+    console.log('File renamed!');
+  });
+
 }).listen(8080);
